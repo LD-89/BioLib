0ab2a5b99382202389447a63b78b35c274fc7297
dariuszlenart <dariusz@lenart-it.pl>
2025-04-09 13:26:38 +0200

    feat: frequency_map and frequent_words methods

--- biolib.py
+++ biolib.py
@@ -1,6 +1,3 @@
-import cmd
-
-
 class BioLib:
     def pattern_count(self, text: str, pattern: str):
         count = 0
@@ -9,29 +6,21 @@ class BioLib:
                 count += 1
         return count
 
+    def frequency_map(self, text: str, pattern_length: int):
+        frequency_map = {}
+        text_length = len(text)
 
-class BioLibCLI(cmd.Cmd):
-    prompt = "BioLib>> "
-    intro = "Welcome to BioLib! Type help to learn more."
-
-    def __init__(self):
-        super().__init__()
-        self.app = BioLib()
-
-    def do_pattern_count(self, text: str, pattern: str):
-        """
-        Find pattern in text
-        """
-        self.app.pattern_count(text, pattern)
-
-    def do_quit(self, line):
-        """Quit the CLI."""
-        return True
-
-    def postcmd(self, stop, line):
-        print()
-        return stop
+        for i in range(text_length-pattern_length-1):
+            pattern = text[i:i+pattern_length]
+            frequency_map[pattern] = frequency_map.get(pattern, 0) + 1
+        return frequency_map
 
 
-if __name__ == '__main__':
-        BioLibCLI().cmdloop()
+    def frequent_words(self, text: str, pattern_length: int):
+        words = []
+        frequency_map = self.frequency_map(text, pattern_length)
+        max_frequency = max(frequency_map.values())
+        for pattern, count in frequency_map.items():
+            if count == max_frequency:
+                words.append(pattern)
+        return words
---
+++ cli.py
@@ -0,0 +1,42 @@
+import cmd
+
+from biolib import BioLib
+
+
+class BioLibCLI(cmd.Cmd):
+    prompt = "BioLib>> "
+    intro = "Welcome to BioLib! Type help to learn more."
+
+    def __init__(self):
+        super().__init__()
+        self.app = BioLib()
+
+    def do_pattern_count(self, text: str, pattern: str):
+        """
+        Find count of pattern occurrences in a text
+        """
+        self.app.pattern_count(text, pattern)
+
+    def do_frequency_map(self, text: str, pattern_length: str):
+        """
+        Map every pattern occurrence in a text
+        """
+        self.app.frequency_map(text, int(pattern_length))
+
+    def do_frequent_words(self, text: str, pattern_length: str):
+        """
+        Get a list of the most frequent words in a text
+        """
+        self.app.frequent_words(text, int(pattern_length))
+
+    def do_quit(self, line):
+        """Quit the CLI."""
+        return True
+
+    def postcmd(self, stop, line):
+        print()
+        return stop
+
+
+if __name__ == '__main__':
+        BioLibCLI().cmdloop()
